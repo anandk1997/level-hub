38fe8851746d4d633a4f4fcfc22a1146fc9dab50
anandk1997 <anandk01997@gmail.com>
2025-03-31 21:36:26 +0530

    fixed resend otp

--- src/components/resendOtp.tsx
+++ src/components/resendOtp.tsx
@@ -1,29 +1,39 @@
 import { Button } from '@mui/material';
-import { useResendOtpMutation } from 'src/slices/apis/app.api';
+import { useForgotPasswordMutation, useResendOtpMutation } from 'src/slices/apis/app.api';
 import toast from 'react-hot-toast';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { getErrorMessage } from 'src/slices/apis/types';
 import { useToastLoading } from 'src/hooks/useToastLoading';
+import { useLocation } from 'react-router-dom';
+import { route } from 'src/utils/constants/routes';
 
 export const ResendOtp = ({ email }: { email: string }) => {
+  const { pathname } = useLocation();
   const [resendTimer, setResendTimer] = useState(60);
   const [isResendDisabled, setIsResendDisabled] = useState(true);
 
   const [resendOtp, { isLoading: isResending }] = useResendOtpMutation();
+  const [forgotPassword, { isLoading }] = useForgotPasswordMutation();
 
-  const handleResendOtp = async () => {
-    if (!email) return toast.error('Email is required');
-
-    const { error, data } = await resendOtp({ email });
+  const handleApiCall = useCallback(async (apiCall: (args: { email: string }) => Promise<any>) => {
+    const { error, data } = await apiCall({ email });
 
     if (error) return toast.error(getErrorMessage(error));
+    if (data?.success) toast.success('OTP resent successfully');
 
-    if (data.success) toast.success('OTP resent successfully');
     setResendTimer(60);
     setIsResendDisabled(true);
+  }, []);
+
+  const handleResendOtp = () => {
+    if (!email) return toast.error('Email is required');
+
+    const apiCall = pathname === route.otpReset ? forgotPassword : resendOtp;
+
+    handleApiCall(apiCall);
   };
 
-  useToastLoading(isResending, 'Resending Otp!');
+  useToastLoading(isResending || isLoading, 'Resending OTP!');
 
   useEffect(() => {
     if (resendTimer > 0) {
@@ -37,7 +47,7 @@ export const ResendOtp = ({ email }: { email: string }) => {
   return (
     <Button
       onClick={handleResendOtp}
-      disabled={isResendDisabled || isResending}
+      disabled={isResendDisabled || isResending || isLoading}
       className="!text-[#FF991F] ml-1 disabled:!text-gray-400"
     >
       {isResendDisabled ? `Resend in ${resendTimer}s` : 'Resend OTP'}
